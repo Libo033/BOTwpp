df1a7cddf60afd2f610e6e90fd38a9a0bff8df0a
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-07-13 22:41:59 -0300

    Añadido control de errores

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { useEffect } from 'react'
-import axios from 'axios'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useEffect } from "react";
+import axios from "axios";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
   useEffect(() => {
-    axios.get("/api/bot")
+    axios
+      .get("/api/bot")
+      .then(() => {
+        console.log("Cargado Correctamente...");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }, []);
 
-  return (
-    <>
-      
-    </>
-  )
+  return <></>;
 }
