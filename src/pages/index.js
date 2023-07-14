4ab8066ac684794a2d49a359daef496a6b0ad623
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-07-13 22:37:32 -0300

    Añadido axios en index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  useEffect(() => {
+    axios.get("/api/bot")
+  }, []);
+
   return (
     <>
       
